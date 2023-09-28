b9bbf0debeddd791e22cb4e0be4cb0085d28bdcf
Matthew Smart <matthewsmart13@gmail.com>
2023-09-28 17:07:39 -0500

    Added basic route for from

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+    res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', function(req, res) {
+    messages.push({
+        text: req.body.message,
+        user: req.body.user,
+        added: new Date(),
+    });
+    res.redirect('/');
+});
+
 module.exports = router;
