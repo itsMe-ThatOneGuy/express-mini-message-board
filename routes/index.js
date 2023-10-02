9beb91d1804968ed0cdd1ca205039a2df500198a
Matthew Smart <matthewsmart13@gmail.com>
2023-10-02 18:40:44 -0500

    Added dotenv

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ const messages = [
     },
 ];
 
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Mini Message Board', messages: messages });
