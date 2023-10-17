d1e6600ffe59b7fcf1f150dec127d5f3c7526aa2
Matthew Smart <matthewsmart13@gmail.com>
2023-10-16 21:33:02 -0500

    Removed bloat

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,8 @@
-/* const messages = [
-    {
-        text: 'Hi there!',
-        user: 'Amando',
-        added: new Date(),
-    },
-    {
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date(),
-    },
-]; */
-
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
 
 router.get('/', function (req, res) {
 	res.redirect('/messages');
 });
-
-/* router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Mini Message Board', messages: messages });
-});
-
-router.get('/new', function (req, res, next) {
-    res.render('form', { title: 'New Message' });
-});
-
-router.post('/new', function (req, res) {
-    messages.push({
-        text: req.body.message,
-        user: req.body.user,
-        added: new Date(),
-    });
-    res.redirect('/');
-}); */
 
 module.exports = router;
