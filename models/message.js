e0fc143028de52ae3ef03ca16c7cd571144ff2e0
Matthew Smart <matthewsmart13@gmail.com>
2023-10-16 21:20:55 -0500

    Updated message model

--- models/message.js
+++ models/message.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    user_name: { type: String, required: true, maxLength: 100 },
-    message: { type: String, requireed: true, maxLength: 100 },
-    date: { type: Date, default: Date.now },
+	user_name: { type: String, required: true, maxLength: 100 },
+	message: { type: String, requireed: true, maxLength: 100 },
+	date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('message', MessageSchema);
+module.exports = mongoose.model('Message', MessageSchema);
