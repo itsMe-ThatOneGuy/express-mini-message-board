3533359207b6e3ec64ec1db406eb0c91eb747732
Matthew Smart <matthewsmart13@gmail.com>
2023-10-17 01:18:21 -0500

    Added fromatted date

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -6,6 +7,10 @@ const MessageSchema = new Schema({
 	user_name: { type: String, required: true, maxLength: 100 },
 	message: { type: String, requireed: true, maxLength: 100 },
 	date: { type: Date, default: Date.now },
+});
+
+MessageSchema.virtual('date_formatted').get(function () {
+	return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
