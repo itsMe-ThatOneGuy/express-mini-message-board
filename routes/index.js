1c46495e041cf1ad5e1c4ef171af62cf45d08717
Matthew Smart <matthewsmart13@gmail.com>
2023-10-16 21:31:31 -0500

    Updated to use DB

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const messages = [
+/* const messages = [
     {
         text: 'Hi there!',
         user: 'Amando',
@@ -9,27 +9,31 @@ const messages = [
         user: 'Charles',
         added: new Date(),
     },
-];
+]; */
 
-require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res) {
+	res.redirect('/messages');
+});
+
+/* router.get('/', function (req, res, next) {
     res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
-router.get('/new', function(req, res, next) {
+router.get('/new', function (req, res, next) {
     res.render('form', { title: 'New Message' });
 });
 
-router.post('/new', function(req, res) {
+router.post('/new', function (req, res) {
     messages.push({
         text: req.body.message,
         user: req.body.user,
         added: new Date(),
     });
     res.redirect('/');
-});
+}); */
 
 module.exports = router;
